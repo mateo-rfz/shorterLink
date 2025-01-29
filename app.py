35af44f09f223185f1a89df069db70d9caf58dc4
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-01-29 22:32:56 +0330

    crate the base of api

--- app.py
+++ app.py
@@ -1,1 +1,38 @@
 from flask import Flask , render_template
+
+#import socket for find local ip
+import socket
+
+
+app = Flask(__name__)
+
+
+@app.route("/" , methods = ["GET" , "POST"])
+def mainPage() : 
+    pass
+
+
+@app.route("/home" , methods = ["GET" , "POST"])
+def home() : 
+    pass
+
+
+@app.route("/createLink" , methods = ["GET" , "POST"])
+def createLink() : 
+    pass
+
+
+@app.route("/login" , methods = ["GET" , "POST"])
+def login() : 
+    pass
+
+
+@app.route("/signUp" , methods = ["GET" , "POST"])
+def signUp():
+    pass
+
+
+
+
+if (__name__) == ("__main__") : 
+    app.run(host=socket.gethostbyname(socket.gethostname()) , port=80)
