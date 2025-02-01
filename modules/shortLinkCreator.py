5d2c0dccd147bf898600f5aacb134607bf45f202
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-02-01 03:46:47 +0330

    change functional struct to oop struct

--- modules/shortLinkCreator.py
+++ modules/shortLinkCreator.py
@@ -1,28 +1,51 @@
 import random
 import string
-# from modules import dbController
-
-
-def createShortLink() :
-    while True : 
-        all_chars = string.ascii_letters + string.digits
-
-        final = ""
-
-        p = random.choices(all_chars , k=5)
-        for char in p : 
-            final = final + char
-
-        shLinks = dbController.allShortLinks()
-        for i in shLinks : 
-            if p == i : 
-                break
-        else : 
-            return p
+from modules import dbController
 
 
 
+class LinkManager : 
+    """
+    
+    """
+    def __init__(self) : 
+        self.allShortLinks = dbController.allShortLinks() #all short links in data base
+        self.allChars = string.ascii_letters + string.digits
 
-def createSpecialLink(suggestLink) : 
-    pass
+
+    def createShortLink() :
+        """
+        
+        """
+        while True : 
+
+            final = ""
+
+            p = random.choices(self.allChars , k=5)
+            for char in p : 
+                final = final + char
+
+            for i in self.allShortLinks : 
+                if p == i : 
+                    break
+            else : 
+                return p
+
+
+    def createSpecialLink(suggestLink) :
+        """
+        
+        """ 
+        for i in self.allShortLinks : 
+            if suggestLink == i : 
+                #check for exists suggest url in data base
+                return False
+
+        return True
+
+
+
+
+
+
 
