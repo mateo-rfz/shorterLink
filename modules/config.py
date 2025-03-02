a7350b312437a2689dee6ae87d02981d49176f28
mateo-rfz <mahdifeyzolahy@gmail.com>
2025-03-02 13:10:22 +0330

    add expire time variable for redis

--- modules/config.py
+++ modules/config.py
@@ -10,3 +10,14 @@ DBPORT = 3306
 #REDIS
 REDISHOST = "localhost"
 REDISPORT = 6379
+EXPIRETIME = 14400 #4h
+"""
+time table for expire qrcode on redis
+
+1h -> 3600
+2h -> 7200
+3h -> 10800
+4h -> 14400
+5h -> 18000
+6h -> 21600
+"""
